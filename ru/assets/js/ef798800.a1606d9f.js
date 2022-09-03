c9873ef86270a385d6a33f9751257cceb92925ff
ookami-kb <ookami.kb@gmail.com>
2022-09-03 23:59:06 +0200

    Deploy website - based on a528e3623e5b00bac4168832026dd667b4c23725

---
+++ ru/assets/js/ef798800.a1606d9f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[2534],{3143:e=>{e.exports=JSON.parse('{"permalink":"/ru/blog","page":1,"postsPerPage":10,"totalPages":3,"totalCount":25,"nextPage":"/ru/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
