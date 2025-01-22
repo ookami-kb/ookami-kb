2fd7ac2c8493a3a7345b3e483ef3730094f7f066
Kirill Bubochkin <ookami.kb@gmail.com>
2025-01-22 17:43:54 +0100

    Deploy website - based on b54f91dc3e6d5595d3044e729fd3a4407a8c67d9

--- ru/assets/js/af9f2c22.f089b926.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[2163],{8972:e=>{e.exports=JSON.parse('{"permalink":"/ru/blog/page/2","page":2,"postsPerPage":10,"totalPages":3,"totalCount":27,"previousPage":"/ru/blog","nextPage":"/ru/blog/page/3","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
